17a720eeee02e88c13b8a596a3b8f3372e136979
jason <jbierbrauer99@gmail.com>
2017-05-06 10:18:19 -0500

    testing git plugin out

--- src/org/launchcode/java/demos/java4python/Hello.java
+++ src/org/launchcode/java/demos/java4python/Hello.java
@@ -1,10 +1,15 @@
 package org.launchcode.java.demos.java4python;
 
+import java.util.*;
+
 /**
  * From "Java for Python Programmers"
  */
 public class Hello {
+	static Scanner scan = new Scanner(System.in);
+	
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+    	String name = scan.nextLine();
+        System.out.printf("Hello, %s, that's a nice shirt you are wearing!\n", name);
     }
 }
